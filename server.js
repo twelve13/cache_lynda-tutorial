7c1fe3660a2557c0c7ce69a4538e17b96e695122
Caroline Chen <carolinecaroline@gmail.com>
2018-11-01 16:41:29 -0400

    initial setup

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import config from "./config";
+import apiRouter from "./api";
 import express from "express";
-import fs from "fs";
 
 const server = express();
 
@@ -29,14 +29,16 @@ server.get('/somepage.html', (req, res) => {
 });
 //in another terminal tab run $curl http://localhost:8080/somepage.html and you should see ^message
 
-server.get('/learningpage.html', (req, res) => {
-	fs.readFile('./learningpage.html', (err, data) => {
-		res.send(data.toString());
-	});
-});
+// server.get('/learningpage.html', (req, res) => {
+// 	fs.readFile('./learningpage.html', (err, data) => {
+// 		res.send(data.toString());
+// 	});
+// });
 
 //don't even need any of that if you do
 server.use(express.static('public'));
+
+server.use("/api", apiRouter);
 
 server.listen(config.port, config.host, () => {
   console.info('Express listening on port', config.port);
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDom.render(
+	React.createElement("h2", null, "hello react"),
+	document.getElementById("root")
+);
